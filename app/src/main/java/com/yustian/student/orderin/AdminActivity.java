9ad02f3c02b9ed2cf3a806561e69e765251ef80e
Yustian Ray <ray.yustian@gmail.com>
2019-07-19 22:53:24 +0700

    Menu Intent

--- app/src/main/java/com/yustian/student/orderin/AdminActivity.java
+++ app/src/main/java/com/yustian/student/orderin/AdminActivity.java
@@ -1,13 +1,31 @@
 package com.yustian.student.orderin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdminActivity extends AppCompatActivity {
+    TextView btn_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        btn_menu = (TextView) findViewById(R.id.btn_menu);
+
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(AdminActivity.this, AdminMenuActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
